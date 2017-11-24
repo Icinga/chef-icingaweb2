657c94aa3f477d0c640ccddc479a9bc1244e777a
Virender Khatri <vir.khatri@gmail.com>
2017-11-24 13:30:57 +0530

    updated apache2 cookbook version

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/Icinga/chef-icingaweb2/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/Icinga/chef-icingaweb2' if respond_to?(:source_url)
 
-depends 'apache2', '~> 5.0.1'
+depends 'apache2', '~> 3.3.0'
 depends 'yum-epel', '>= 2.1.1'
 depends 'apt', '>= 5.0.1'
 depends 'icinga2repo', '>= 1.0.0'
